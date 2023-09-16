d049bc2794ea8472f319299002d2499807f57aa5
unknown <Priyanshyadav1012@gmail.com>
2023-09-16 13:11:46 +0530

    Native wind added and downgrade tailwindcss version

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello! React Native.</Text>
-      <StatusBar style="auto" />
+    <View className = "flex-1 items-center justify-center bg-black">
+      <Text className= " text-slate-300" >Hello! React Native.</Text>
+      <StatusBar/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    fontSize:27 ,
-    fontFamily: 'bold',
-  }
-});
