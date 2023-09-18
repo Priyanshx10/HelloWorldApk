eb2b9043ba2a66fc861f4de6270b4cb96b039428
unknown <Priyanshyadav1012@gmail.com>
2023-09-18 16:50:05 +0530

    Convert the files into tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { Text, View } from 'react-native';
 import React from 'react';
 
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View className = "flex-1 items-center justify-center bg-black">
       <Text className= " text-slate-300" >Hello! React Native.</Text>
-      <StatusBar/>
+
     </View>
   );
 }
